2922b4e8c797c31ce0fb3c18148042c992b872fb
Ante Pocedulic <ante@ante003.com>
2013-08-13 23:40:21 +0200

    minor mistake fixed

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #/usr/bin/bash
 
-path= YOUR PATH
+path=/mnt/data/projekti/prognoza/
 cd $path;
 
 rm forecast_hour_by_hour.xml*
 wget http://www.yr.no/place/Croatia/Grad_Zagreb/Zagreb/forecast_hour_by_hour.xml; 
-cat conky_template > conky2.conf; 
-python prognoza.py >> conky2.conf;
+cat conky_template > conky.conf; 
+python prognoza.py >> conky.conf;
 
 
 cd;
